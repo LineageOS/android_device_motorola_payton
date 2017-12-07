f6749edf88f47481724959d24121b4fee3bbd1a3
Andre Eisenbach <eisenbach@google.com>
2017-12-06 16:22:22 -0800

    payton: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Test: manual
    Merged-In: I213034b54724b1ed0f43b32bc757ea7d8873ee74
    Change-Id: I213034b54724b1ed0f43b32bc757ea7d8873ee74
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Recovery
