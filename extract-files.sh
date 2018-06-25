478fa8fadd0ba4a0775e59b9ed6b66a978284ac8
Erfan Abdi <erfangplus@gmail.com>
2018-06-25 20:25:31 +0430

    payton: Move more camera blobs to vendor
    * libSonyDefocus and ZAF configs
    
    Change-Id: I87bb12d041476df3e62a9573915e1618553b7c03
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -17,6 +17,12 @@
 
 set -e
 
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
 export DEVICE=payton
 export DEVICE_COMMON=sdm660-common
 export VENDOR=motorola
@@ -24,3 +30,13 @@ export VENDOR=motorola
 export DEVICE_BRINGUP_YEAR=2018
 
 ./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+
+BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+# Load libSonyDefocus from vendor
+CAMERA_IMX386="$BLOB_ROOT"/vendor/lib/libmmcamera_imx386.so
+sed -i "s|/system/lib/hw/|/vendor/lib/hw/|g" "$CAMERA_IMX386"
+
+# Load ZAF configs from vendor
+ZAF_CORE="$BLOB_ROOT"/vendor/lib/libzaf_core.so
+sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "$ZAF_CORE"
