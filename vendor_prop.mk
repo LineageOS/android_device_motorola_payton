d94dd6128078e813dd69e6b74a74b5010158b9ba
SyberHexen <SyberHexen@gmail.com>
2019-03-26 16:01:32 -0700

    payton: point audio calibractions to vendor
    Change-Id: I1ffe5725dfbaab885873de93c79c1ee167b7ba7b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -4,13 +4,13 @@
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.audio.calfile0=/vendor/etc/acdbdata/Bluetooth_cal.acdb \
-    persist.audio.calfile1=/vendor/etc/acdbdata/General_cal.acdb \
-    persist.audio.calfile2=/vendor/etc/acdbdata/Global_cal.acdb \
-    persist.audio.calfile3=/vendor/etc/acdbdata/Handset_cal.acdb \
-    persist.audio.calfile4=/vendor/etc/acdbdata/Hdmi_cal.acdb \
-    persist.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
-    persist.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb
+    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/Bluetooth_cal.acdb \
+    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/General_cal.acdb \
+    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/Global_cal.acdb \
+    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/Handset_cal.acdb \
+    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/Hdmi_cal.acdb \
+    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
+    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
