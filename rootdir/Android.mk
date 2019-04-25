4180775a3a14a5ba4fb5c27535cee8d39f02229b
padarshr <padarshr@codeaurora.org>
2019-04-25 17:58:17 +0530

    payton: Address 'LOCAL_MODULE_TAGS := eng debug'  deprecation.
    Newer AOSP deprecates LOCAL_MODULE_TAGS := eng debug.
    We now remove that and instead add the package to
    PRODUCT_PACKAGES_ENG or PRODUCT_PACKAGES_DEBUG as applicable.
    
    To ensure that we don't miss out on any package during this switch,
    we add these explicitly, even though it might have been already
    added under PRODUCT_PACKAGES at some other location.
    
    Change-Id: I35bd67c3e7752be5bcd8f8682864062173e92724

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.payton.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.payton.rc
 LOCAL_VENDOR_MODULE    := true
