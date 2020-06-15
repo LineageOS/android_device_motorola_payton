ddcb4c70188e3e26bf21440f86a61327c7a2916e
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-15 09:13:07 -0400

    payton: Commonize bluetooth configuration
    Change-Id: I708a0f3e59d58958b43edc16a8c9c34a62efbe80

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,9 +22,6 @@ DEVICE_PATH := device/motorola/payton
 # Assertions
 TARGET_OTA_ASSERT_DEVICE := payton
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-
 # Display
 TARGET_SCREEN_DENSITY := 420
 
