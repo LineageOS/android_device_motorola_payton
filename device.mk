10f38ee32fa61a58498c0ac81789e47c81d18264
Erfan Abdi <erfangplus@gmail.com>
2020-07-28 00:08:09 +0430

    payton: Build PocketMode app
    Change-Id: I7e9b03a8c139ff80fa99b8dcc317ab548943ebf5

--- device.mk
+++ device.mk
@@ -92,6 +92,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    MotoPocketMode
+
 # Recovery
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.payton.rc:root/init.recovery.payton.rc
