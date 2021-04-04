e2e51a96b2ef53df7b94e39d98c268627ff5fb13
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-04-03 21:41:44 -0400

    payton: Import fluence props from sdm660-common
    - Fluence props were decommonized.
    
    [npjohnson]: Keep payton's local additions.
    
    Change-Id: I950b03206adcefbf4a60e4effcaef385cc6029ad

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -12,9 +12,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
     persist.vendor.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb
 
+# Fluence
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.dualmic.config=endfire \
+    persist.vendor.audio.fluence.audiorec=false \
     persist.vendor.audio.fluence.speaker=true \
-    persist.vendor.audio.fluence.voicecall=true
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicecomm=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    ro.qc.sdk.audio.fluencetype=none \
+    ro.vendor.audio.sdk.fluencetype=none
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
