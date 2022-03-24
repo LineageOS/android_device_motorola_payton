cc5f5b007d2b6b5262cdcaaebb573e3abba4ff8e
Jeferson <jroliveira.oliveira301@gmail.com>
2022-03-24 13:43:32 +0100

    payton: Squashed inclusion of MotCamera2
    This is a squashed commit of:
    
    Add Moto Camera 2 & priv-app permissions
    * Snap APIv2 doesn't support our Aux Cam, or Camera
      Mods (considering they are removable and need special
      HAL handling for flash, internal Camera/USB disabling,
      etc.
    
    * Snap APIv1 doesn't like our front-flash, or video
      recording audio (likely fixable). As well as the
      aforementioned lack of Camera Mod support.
    
    * Mata ships Klik to support an external Camera "mod"
      and sets precedent for shipping OEM Cameras, so we're
      just going to go ahead and do it.
    
    Change-Id: I7d5001e7f743cdb9aaeb70c032a19cc008209135
    ---------------------------------------------------------
    
    Mark MotCamera2 as presigned
    Change-Id: Ifa4501b9dc14326ce73df1b106a9a0dcd5f9b5f6
    ---------------------------------------------------------
    
    Include MotoSignatureApp
    * Needed for MotCamera2 to update via the Play Store.
    * Also exclude from dex-opt'ing on host, as this app
      is known not to like that (see: shamu, clark).
    
    Change-Id: Ibf2aafd731783fdae63b228460753457296de47c
    ----------------------------------------------------------
    
    Pin MotCamera2 current version
    Change-Id: I01afda4e67f91f0c21858ec617655a30203e1ed7
    ----------------------------------------------------------
    
    Update MotCamera2 to EOL version
    Change-Id: I3dd0a1534ab6f345da696ecfb625a5e09530b7d6
    ----------------------------------------------------------
    
    Separate JNI libs for MotCamera2 from APK
    Remove JNI libraries from APK and put them under
    /system/priv-app/MotCamera2/lib in order to reduce
    size of MotCamera2.apk.
    
    Signed-off-by: Ihor Ivanov <toulmee@gmail.com>
    Change-Id: I54bda3fe7bfce1c34ad87aec6c51510716fd7ef3
    ----------------------------------------------------------
    
    Override Camera2 with MotCamera2 properly
    * Possible as of https://review.lineageos.org/c/LineageOS/android_tools_extract-utils/+/314698.
    
    Change-Id: I88808f7ab795099dc0ea1e80992d7c68aaa90167
    ---------------------------------------------------------
    
    Change-Id: I89aa823bac2a2fa4b92d324cc0dbcdc6811468de

--- device.mk
+++ device.mk
@@ -42,6 +42,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Dex-pre-opt exclusions
+$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
+
 # Fingerprint Gestures
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
