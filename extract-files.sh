c52cb4f8ada8061bee95e39ecdde7b4151ac3111
Georg Veichtlbauer <georg@vware.at>
2023-04-15 10:17:00 +0200

    payton: Switch common repo to msm8998-common
    Change-Id: I6d6a8bb1cd9ed272513c54aa10ecc39c9b5dc158

--- extract-files.sh
+++ extract-files.sh
@@ -32,7 +32,7 @@ fi
 set -e
 
 export DEVICE=payton
-export DEVICE_COMMON=sdm660-common
+export DEVICE_COMMON=msm8998-common
 export VENDOR=motorola
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
