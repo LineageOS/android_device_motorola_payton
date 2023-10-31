224bfc0b1cc4d210f91fa44e1b27eff438eb896a
LuK1337 <priv.luk@gmail.com>
2023-10-31 19:06:44 +0100

    payton: Get rid of AVB 1.0
    It's dead in U.
    
    Change-Id: Id948fe50fd358837cafa3b1e18cf2adba32b927b

--- device.mk
+++ device.mk
@@ -111,11 +111,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/oem
-$(call inherit-product, build/target/product/verity.mk)
-
 # VNDK
 PRODUCT_SHIPPING_API_LEVEL := 25
 
