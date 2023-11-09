9c51fb903939b160fec730bff9570c554718ac1d
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-09 16:18:45 -0500

    fixup! payton: Use libhidlbase-v32 for fingerprint libs
    Change-Id: I05f7672fee8c9fcf8b799472b63d1d009ad1e5db

--- extract-files.sh
+++ extract-files.sh
@@ -21,7 +21,7 @@ function blob_fixup() {
             sed -i -e 's|ro.product.manufacturer|ro.product.nopefacturer|g' "${2}"
             ;;
         # Fix missing symbol _ZN7android8hardware7details17gBnConstructorMapE
-        vendor/lib64/com.fingerprints.extension@1.0.so)
+        vendor/lib64/com.fingerprints.extension@1.0_vendor.so)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
     esac
