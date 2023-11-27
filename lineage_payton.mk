ec20a127729338d19bafdb5af96052fa15487916
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-27 01:44:48 -0500

    payton: Restructure tree to match nash
    Change-Id: Ibd9953004a13f8264897a4c8e090448cd96eb58b

--- lineage_payton.mk
+++ lineage_payton.mk
@@ -1,62 +1,19 @@
-#
-# Copyright (C) 2017 The LineageOS Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Device
 $(call inherit-product, device/motorola/payton/device.mk)
 
-# A/B updater
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-# The following modules are included in debuggable builds only.
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl \
-    update_engine_client
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.sdm660 \
-    bootctrl.sdm660.recovery
-
 # Device identifiers
-PRODUCT_DEVICE := payton
-PRODUCT_NAME := lineage_payton
+BUILD_FINGERPRINT := motorola/payton/payton:8.0.0/OPWS27.57-25-6-10/12:user/release-keys
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := Moto X4
+PRODUCT_DEVICE := payton
 PRODUCT_MANUFACTURER := Motorola
+PRODUCT_MODEL := Moto X4
+PRODUCT_NAME := lineage_payton
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=payton \
         PRIVATE_BUILD_DESC="payton-user 8.0.0 OPWS27.57-25-6-10 12 release-keys"
-
-BUILD_FINGERPRINT := motorola/payton/payton:8.0.0/OPWS27.57-25-6-10/12:user/release-keys
