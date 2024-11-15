7546d8e8702faef7f129a51635627fa2bdbfa751
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-15 00:27:48 -0500

    payton: Commonize libhidlbase-v32
    Change-Id: Ic6757897e52b7783ee2f58ff84edee4ec842ced1
    (cherry picked from commit 67c9f4f573d257b9b9e299f5dae66e0169364fb9)

--- device.mk
+++ device.mk
@@ -139,10 +139,6 @@ PRODUCT_PACKAGES_DEBUG += \
     bootctl \
     update_engine_client
 
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
-
 # Inherit from motorola msm8998-common
 TARGET_MOTO_PLATFORM := sdm660
 $(call inherit-product, device/motorola/msm8998-common/common.mk)
