26fdfd7e2faab52c87275b8e7ff7135e33353a37
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-28 01:31:29 -0400

    payton: Implement ELF checks & swap to python extract
    * Resolve all possible ELF checks.
    * Use OSS RE'd fingerprint extensions in msm8998-common.
    * Commonize libgralloc1 for dependencies namespacing.
    * Decommonize several DRM/Camera (SCVE) libraries and
      charge_only_mode for dependencies namespacing.
    * Kill unused libmmcamera_chromaflash_lib.so, libmmcamera_llvd.so
      libmmcamera_optizoom_lib.so, libmmcamera_stillmore_lib.so,
      libmmcamera_trueportrait_lib.so.
    * Kang new DRM deps from lake as our device has a weirdly old
      DRM stack.
    * Kang Moto Signature from lake... some random build system
      dex pre-opt error I don't care to dig into.
    * sort-blobs-list.
    
    Change-Id: I6ce52a93f7ce21fb2eeebbd6ea76009313787e02

---
+++ extract-files.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/motorola/payton',
+    'device/motorola/msm8998-common',
+    "hardware/qcom-caf/msm8998",
+    "hardware/qcom-caf/wlan",
+    'vendor/motorola/msm8998-common',
+    "vendor/qcom/opensource/dataservices",
+]
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/charge_only_mode': blob_fixup()
+        .add_needed('libmemset_shim.so'),
+    'vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc': blob_fixup()
+        .regex_replace('system input', 'system uhid input'),
+    (
+        'vendor/lib/libdualcameraddm.so',
+        'vendor/lib/libmmcamera_hdr_gb_lib.so',
+        'vendor/lib/libvideobokeh.so',
+        'vendor/lib64/libdualcameraddm.so',
+        'vendor/lib64/libvideobokeh.so'
+    ): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib/libmmcamera2_pproc_modules.so': blob_fixup()
+        .binary_regex_replace(b'\x70\x72\x6F\x64\x75\x63\x74\x2E\x6D\x61\x6E\x75', b'\x70\x72\x6F\x64\x75\x63\x74\x2E\x6E\x6F\x70\x65'),
+    'vendor/lib/libzaf_core.so': blob_fixup()
+        .binary_regex_replace(b'\x2f\x73\x79\x73\x74\x65\x6d\x2f\x65\x74\x63\x2f\x7a\x61\x66', b'\x2f\x76\x65\x6e\x64\x6f\x72\x2f\x65\x74\x63\x2f\x7a\x61\x66'),
+    'vendor/lib64/vendor.qti.hardware.tui_comm@1.0.so': blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'payton',
+    'motorola',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'msm8998-common', module.vendor)
+    utils.run()
