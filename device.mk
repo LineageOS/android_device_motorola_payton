34424a7cf609f47d27246961c3a87fc5fd4c38e5
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    payton: Migrate to AIDL LiveDisplay SDM/sysfs HALs
    Change-Id: I580e0721b050480ed5a9ca8bb145f9ef1f1b174f

--- device.mk
+++ device.mk
@@ -87,6 +87,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.msm8998
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set,livedisplay_sysfs,enable_ab,true)
+$(call soong_config_set,livedisplay_sysfs,enable_se,true)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
